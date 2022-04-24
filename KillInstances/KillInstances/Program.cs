613d4eded6be13320664abf6f760ce6824b10213
Adam Schneider <32423965+AWSchneider-dev@users.noreply.github.com>
2022-04-24 00:08:19 -0400

    adding files

---
+++ KillInstances/KillInstances/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KillInstances
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Application.EnableVisualStyles();
+          Application.SetCompatibleTextRenderingDefault(false);
+
+          LowLevelKeyBoardHook kbh = new LowLevelKeyBoardHook();
+          KeyPressPowerShellEventHandler keyPressHanlder = new KeyPressPowerShellEventHandler();
+          kbh.OnKeyPressed += keyPressHanlder.kbh_OnKeyPressed;
+          kbh.OnKeyUnpressed += keyPressHanlder.kbh_OnKeyUnPressed;
+          kbh.HookKeyboard();
+
+          Application.Run();
+
+          kbh.UnHookKeyboard();
+        }
+    }
+}
